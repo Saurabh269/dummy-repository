b65326a8c464851592d5b858bdee48e72664cbaf
Saurabh <s276612@gmail.com>
2022-02-22 21:41:32 +0530

    new spring-project created

---
+++ src/main/java/com/springbasics/controller/MyController.java
@@ -0,0 +1,16 @@
+package com.springbasics.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@GetMapping("/name")
+	//@ResponseBody
+	public String getString() {
+		return "Hello";
+	}
+}
