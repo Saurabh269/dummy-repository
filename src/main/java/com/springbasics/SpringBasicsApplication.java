596dbdbab80d99e0e039101257cab583bcfd1b2b
Saurabh <s276612@gmail.com>
2022-02-23 13:48:06 +0530

    comment removed from main class

--- src/main/java/com/springbasics/SpringBasicsApplication.java
+++ src/main/java/com/springbasics/SpringBasicsApplication.java
@@ -3,7 +3,7 @@ package com.springbasics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//main class
+
 @SpringBootApplication
 public class SpringBasicsApplication {
 
