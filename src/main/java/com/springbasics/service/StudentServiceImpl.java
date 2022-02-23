2c5602219bde2b28d9ad22c864efcc41dbd2a39b
Saurabh <s276612@gmail.com>
2022-02-23 16:48:27 +0530

    adding comment for student service

--- src/main/java/com/springbasics/service/StudentServiceImpl.java
+++ src/main/java/com/springbasics/service/StudentServiceImpl.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Service;
 
 import com.springbasics.entity.Student;
 
+/*
+ * Student Service
+ */
 @Service
 public class StudentServiceImpl {
 
@@ -13,6 +16,7 @@ public class StudentServiceImpl {
 		student = new Student(100, "Saurabh");
 	}
 
+	//get student details
 	public Student get() {
 		return student;
 	}
